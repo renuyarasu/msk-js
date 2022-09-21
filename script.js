b24559ebdad62bd824adeac768564976ed711d79
Renuka Prasad <renu.yarasu@gmail.com>
2022-09-21 14:40:12 +0530

    rest parameters

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // MSK Javascript Bootcamp
 console.clear();
+
+function myFun(a, b, ...moreArgs) {
+    console.log('a', a);
+    console.log('b', b);
+    console.log('moreArgs', moreArgs);
+}
+myFun(1,2,3,4,5,6)
