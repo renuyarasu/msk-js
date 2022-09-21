93eee91151d6a3d4afe7a9d99ee882b45be68cb2
Renuka Prasad <renu.yarasu@gmail.com>
2022-09-21 14:44:16 +0530

    spread operator

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // MSK Javascript Bootcamp
 console.clear();
 
-function myFun(a, b, ...moreArgs) {
-    console.log('a', a);
-    console.log('b', b);
-    console.log('moreArgs', moreArgs);
-}
-myFun(1,2,3,4,5,6)
+let numbers = [0, 1, 2, 3, 4, 5];
+let newNumbers = [6, 7, 8, 9];
+
+let numberStore = [...numbers, ...newNumbers];
+console.log(numberStore);
